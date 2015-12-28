fd915d4fdd71c8fa64afcc0599dc3e76f440832f
codename <daction-z@hotmail.com>
2015-12-27 21:52:18 -0800

    First commit

---
+++ Input and Output/Input and Output/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Input and Output
+//
+//  Created by David Zhong on 2015-12-27.
+//  Copyright © 2015 David Zhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
